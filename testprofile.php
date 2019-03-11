26f621aa9f697eb379c2a8ca006e8c7b2f7df7a9
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-11 11:31:37 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -1,4 +1,6 @@
 <?php 
+
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<c70f0350f357af8e48b1d407eaf05db1>']);
 $response = $bot->getProfile('<U5e9acf1216646459855f5735a974b170>');
@@ -7,5 +9,4 @@ if ($response->isSucceeded()) {
     echo $profile['displayName'];
     echo $profile['pictureUrl'];
     echo $profile['statusMessage'];
-}
 }
