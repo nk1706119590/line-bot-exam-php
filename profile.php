4e5c79b4ea0f7a4b916ff269e6791cffe91e3f43
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-12 14:39:06 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,45 +3,20 @@
 
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 
-$content = file_get_content('php://input');
-$arrayJson = json_decode($content, true);
-//$userId = 'U04116f129718a17c788ee0654836a813';
+$userId = 'U04116f129718a17c788ee0654836a813';
 
-//$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+$url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
-$headers = array();
-  $arrayHeader[] = "Content-Type: application/json";
-  $arrayHeader[] = "Authorization: Bearer {$access_token}";
+$headers = array('Authorization: Bearer ' . $access_token);
 
-$message = $arrayJson['events'][0]['message']['text'];
 
-if(isset($arrayJson['events'][0]['source']['userId']){
-      $id = $arrayJson['events'][0]['source']['userId'];
-}
 
-#ตัวอย่าง Message Type "Text + Sticker"
-   if($message == "สวัสดี"){
-      $arrayPostData['to'] = $id;
-      $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-      $arrayPostData['messages'][1]['type'] = "sticker";
-      $arrayPostData['messages'][1]['packageId'] = "2";
-      $arrayPostData['messages'][1]['stickerId'] = "34";
-      pushMsg($arrayHeader,$arrayPostData);
-   }
-function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
-
-$ch = curl_init($strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+$ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-exit;
+echo $result;
 
