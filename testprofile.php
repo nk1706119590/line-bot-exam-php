cf3df92215ff3fc1939397590695ab7f70cf6c86
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-12 14:48:57 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -73,6 +73,8 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close ($ch);
+    
+    echo $id;
     }
    exit;
 ?>
