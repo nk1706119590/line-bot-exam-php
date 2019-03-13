f40d737e2522374709ed6caa7c55bed28866aa49
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 08:50:04 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,12 @@
 
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U04116f129718a17c788ee0654836a813';
+//$userId = 'U04116f129718a17c788ee0654836a813';
+
+$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
 
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
@@ -40,9 +45,6 @@ if(!is_null($events)){
     }   
 
 
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
-
-$headers = array('Authorization: Bearer ' . $access_token);
 
 
 
