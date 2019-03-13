fab87733ee902183b9cf2dc4bb58c76156e06625
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 08:48:08 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -15,7 +15,30 @@ if(!is_null($events)){
     $sourceType = $events['events'][0]['source']['type'];
     $is_postback = NULL;
     $is_message = NULL;
-}
+    if(isset($events['events'][0]) && array_key_exists('message',$events['events'][0])){
+        $is_message = true;
+        $typeMessage = $events['events'][0]['message']['type'];
+        $userMessage = $events['events'][0]['message']['text'];     
+        $idMessage = $events['events'][0]['message']['id']; 
+    }
+    if(isset($events['events'][0]) && array_key_exists('postback',$events['events'][0])){
+        $is_postback = true;
+        $dataPostback = NULL;
+        parse_str($events['events'][0]['postback']['data'],$dataPostback);;
+        $paramPostback = NULL;
+        if(array_key_exists('params',$events['events'][0]['postback'])){
+            if(array_key_exists('date',$events['events'][0]['postback']['params'])){
+                $paramPostback = $events['events'][0]['postback']['params']['date'];
+            }
+            if(array_key_exists('time',$events['events'][0]['postback']['params'])){
+                $paramPostback = $events['events'][0]['postback']['params']['time'];
+            }
+            if(array_key_exists('datetime',$events['events'][0]['postback']['params'])){
+                $paramPostback = $events['events'][0]['postback']['params']['datetime'];
+            }                       
+        }
+    }   
+
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
