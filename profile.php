21dfa9a6776e3c524b69097b0076294faa828755
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 11:22:25 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,29 +1,19 @@
 <?php
 
-$access_token = 
-
-'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKs
-
-h+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 $cs ='c70f0350f357af8e48b1d407eaf05db1';
 
-$content = 
-
-file_get_contents('php://input');
+$content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 $userId = 'U5e9acf1216646459855f5735a974b170 ';
 
-$url = 
-
-'https://api.line.me/v2/bot/profile/'. $userId;
+$url = 'https://api.line.me/v2/bot/profile/'. $userId;
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
-curl_setopt
-
-($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
