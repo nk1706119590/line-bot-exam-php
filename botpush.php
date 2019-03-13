f988fc1d5b66fb4112c2359f525f654b9fa70693
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 13:09:22 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -13,13 +13,13 @@ $pushID = 'U5e9acf1216646459855f5735a974b170';
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
-$message = $arrayJson['events'][0]['message']['text'];
+/*$message = $arrayJson['events'][0]['message']['text'];
 
 if(isset($arrayJson['events'][0]['source']['userId']){
       $id = $arrayJson['events'][0]['source']['userId'];
    }
 
-/*if(!is_null($events)){
+if(!is_null($events)){
     $replyToken = $events['events'][0]['replyToken'];
     $userID = $events['events'][0]['source']['userId'];
     $sourceType = $events['events'][0]['source']['type'];
@@ -29,7 +29,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 
-$response = $bot->getProfile($id);
+$response = $bot->getProfile($pushID);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo "UserID : " .$profile['userId']."<br>";
