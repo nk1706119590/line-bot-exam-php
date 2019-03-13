c9f7b816c75fe52f5dbf4b3b9dcb548a69b8500b
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 09:55:24 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,9 +2,16 @@
 
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 
+
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+
 $userId = 'U04116f129718a17c788ee0654836a813';
+
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
-$headers = array('Authorization: Bearer ' . $httpClient);
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -13,4 +20,5 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+
+echo "Name".$result['displayName'];
