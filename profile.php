b4e3b082e79ef72780b9eb8b39293cba7dd552c4
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 10:01:46 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -10,7 +10,7 @@ $events = json_decode($content, true);
 
 //$url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
-$headers = array('Authorization: Bearer ' . $access_token);
+//$headers = array('Authorization: Bearer ' . $access_token);
 
 if (!is_null($events['events'])) {// Loop through each event
             foreach ($events['events'] as $event) {// Reply only when message sent is in 'text' format
@@ -21,13 +21,22 @@ if (!is_null($events['events'])) {// Loop through each event
                         'type' => 'text',
                         'text' => $text
                     ];// Make a POST Request to Messaging API to reply to sender
-                    $url = 'https://api.line.me/v2/bot/profile/'.$content;
+                    $url = 'https://api.line.me/v2/bot/message/reply';
                     $data = [
                         'replyToken' => $replyToken,
                         'messages' => [$messages],
                     ];
-                    
+                    $post = json_encode($data);
                     $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                        $ch = curl_init($url);
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                        $result = curl_exec($ch);
+                        curl_close($ch);
+                        echo $result . "\r\n";
                 }
             }
         }
@@ -35,16 +44,6 @@ if (!is_null($events['events'])) {// Loop through each event
 
 
 
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-
-echo $result;
 
 
 
