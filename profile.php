403df48f91276d8e86f3ad1a857c714f98158753
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 09:14:12 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
-
+//$access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>');
 //$bot = new \LINE\LINEBot($access_token, ['channelSecret' => '<c70f0350f357af8e48b1d407eaf05db1>'])
 
 $content = file_get_contents('php://input');
@@ -11,7 +11,7 @@ $userId = 'U04116f129718a17c788ee0654836a813';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . $httpClient);
 
 
 $ch = curl_init($url);
