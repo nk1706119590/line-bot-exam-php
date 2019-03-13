0dd9b70c7d4b0fb365a43b89998af30fe02955c4
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 09:58:26 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,19 +6,51 @@ $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
-$userId = 'U04116f129718a17c788ee0654836a813';
+//$userId = 'U04116f129718a17c788ee0654836a813';
 
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+//$url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
+if (!is_null($events['events'])) {// Loop through each event
+            foreach ($events['events'] as $event) {// Reply only when message sent is in 'text' format
+                if ($event['type'] == 'message' && $event['message']['type'] == 'text') {// Get text sent
+                    $text = $event['source']['userId'];// Get replyToken
+                    $replyToken = $event['replyToken'];// Build message to reply back
+                    $messages = [
+                        'type' => 'text',
+                        'text' => $text
+                    ];// Make a POST Request to Messaging API to reply to sender
+                    $url = 'https://api.line.me/v2/bot/profile/'.$content;
+                    $data = [
+                        'replyToken' => $replyToken,
+                        'messages' => [$messages],
+                    ];
+                    $post = json_encode($data);
+                    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                    $ch = curl_init($url);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                    $result = curl_exec($ch);
+                    curl_close($ch);
+                }
+            }
+        }
+    echo "OK";
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
 
 
-echo "Name".$result['displayName'];
+
+
+
+
+
+
+
+
+
+
+
+
+
