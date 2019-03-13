6944ab8874fc34675aaed54f2c04581f74e1d278
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 11:21:24 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,24 +1,35 @@
 <?php
 
-date_default_timezone_set("Asia/Bangkok");
+$access_token = 
 
-$date = date("Y-m-d");
-$time = date("H:i:s");
+'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKs
 
-$json = file_get_contents('php://input');
-                          
-$request = json_decode($json, true);
-                          
-$queryText = $request["queryResult"]["queryText"];
-$userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
-$myfile = fopen("log$date.txt", "a") or die("Unable to open file!");
-                          
-$log = $date."-".$time."\t".$userId."\t".$queryText."\n";
-fwrite($myfile,$log);
-fclose($myfile);
+h+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
+$cs ='c70f0350f357af8e48b1d407eaf05db1';
 
-?>
+$content = 
 
+file_get_contents('php://input');
+$events = json_decode($content, true);
+
+$userId = 'U5e9acf1216646459855f5735a974b170 ';
+
+$url = 
+
+'https://api.line.me/v2/bot/profile/'. $userId;
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt
+
+($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
 
 
 
