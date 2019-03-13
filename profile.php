d5800b8cdf71631d302371474baa6bce3e8d5d3c
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 10:03:13 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -15,7 +15,8 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {// Loop through each event
             foreach ($events['events'] as $event) {// Reply only when message sent is in 'text' format
                 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {// Get text sent
-                    $text = $event['source']['userId'];// Get replyToken
+                    //$text = $event['source']['userId'];// Get replyToken
+                    $text = $event['source']['displayName'];
                     $replyToken = $event['replyToken'];// Build message to reply back
                     $messages = [
                         'type' => 'text',
