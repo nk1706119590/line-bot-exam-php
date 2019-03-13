375096406dd1ce2428206de50eac2b78b109dbfa
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 14:42:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
+$accessToken = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=";
+
 $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
 
+
+$arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
 $id = $arrayJson['events'][0]['source']['userId'];
 
-
-echo $id;
+$ch = curl_init();
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+echo $result;
