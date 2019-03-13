c26d4f1d1c3a579889a72ee5a2855d153d71fb01
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 09:39:50 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,13 +1,12 @@
 <?php
+
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
-
-$url = 'https://api.line.me/v2/bot/profile/{U3e33538dbb83f5edc83ab0e08aacc144}'
-
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+$userId = 'U04116f129718a17c788ee0654836a813';
+$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+$headers = array('Authorization: Bearer ' . $httpClient);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -17,5 +16,3 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
-?>
-
