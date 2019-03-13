41279a65790f138af96c69a44fd2278bb8a9808a
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 14:12:36 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand
 
 $channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
 
-$pushID = 'U5e9acf1216646459855f5735a974b170';
+$pushID = 'U3e33538dbb83f5edc83ab0e08aacc144 ';
 
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
