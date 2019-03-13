ac227d975382ccd2547e8db89d36d322f7e6dd06
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-13 09:10:05 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,48 +3,23 @@
 
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 
-//$userId = 'U04116f129718a17c788ee0654836a813';
+$bot = new \LINE\LINEBot($access_token, ['channelSecret' => '<c70f0350f357af8e48b1d407eaf05db1>'])
+
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+$userId = 'U04116f129718a17c788ee0654836a813';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
-
-$content = file_get_contents('php://input');
-$events = json_decode($content, true);
-
-if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-    $replyToken = $events['events'][0]['replyToken'];
-    $userID = $events['events'][0]['source']['userId'];
-    $sourceType = $events['events'][0]['source']['type'];
-    $is_postback = NULL;
-    $is_message = NULL;
-    if(isset($events['events'][0]) && array_key_exists('message',$events['events'][0])){
-        $is_message = true;
-        $typeMessage = $events['events'][0]['message']['type'];
-        $userMessage = $events['events'][0]['message']['text'];     
-        $idMessage = $events['events'][0]['message']['id']; 
-    }
-    if(isset($events['events'][0]) && array_key_exists('postback',$events['events'][0])){
-        $is_postback = true;
-        $dataPostback = NULL;
-        parse_str($events['events'][0]['postback']['data'],$dataPostback);;
-        $paramPostback = NULL;
-        if(array_key_exists('params',$events['events'][0]['postback'])){
-            if(array_key_exists('date',$events['events'][0]['postback']['params'])){
-                $paramPostback = $events['events'][0]['postback']['params']['date'];
-            }
-            if(array_key_exists('time',$events['events'][0]['postback']['params'])){
-                $paramPostback = $events['events'][0]['postback']['params']['time'];
-            }
-            if(array_key_exists('datetime',$events['events'][0]['postback']['params'])){
-                $paramPostback = $events['events'][0]['postback']['params']['datetime'];
-            }                       
-        }
-    }   
-
-
+$response = $bot->getProfile(.$userId);
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
+}
 
 
 
