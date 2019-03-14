8daf005e9d608b65ab8caba8bbf4af126de4bd6e
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 09:11:52 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -12,10 +12,7 @@ $arrayJson = json_decode($content, true);
 $arrayHeader = array();
    $arrayHeader[] = "Content-Type: application/json";
    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-   //รับข้อความจากผู้ใช้
-   
-$message = $arrayJson['events'][0]['message']['text'];
-   //รับ id ของผู้ใช้
+
    
 $id = $arrayJson['events'][0]['source']['userId'];
    #ตัวอย่าง Message Type "Text + Sticker"
@@ -24,11 +21,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 
-
-
-
-if($message == "สวัสดี"){
-      $response = $bot->getProfile($id);
+$response = $bot->getProfile($id);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo "UserID : " .$profile['userId']."<br>";
@@ -37,23 +30,6 @@ if ($response->isSucceeded()) {
     echo "Status : " .$profile['statusMessage'];
       
 }
-   
-}
   
-function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,$strUrl);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $result = curl_exec($ch);
-      curl_close ($ch);
-   
-}
 
-   exit;
 ?>
