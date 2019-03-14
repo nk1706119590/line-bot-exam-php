deab3bb8e9a5fcfa95d8899b6b2795a6bc8c69e4
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 09:07:54 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -1,7 +1,10 @@
 <?php
-   
+
+require "vendor/autoload.php";
+
 $accessToken = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-   
+$channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
+
 $content = file_get_contents('php://input');
 $arrayJson = json_decode($content, true);
 
@@ -17,6 +20,8 @@ $message = $arrayJson['events'][0]['message']['text'];
 $id = $arrayJson['events'][0]['source']['userId'];
    #ตัวอย่าง Message Type "Text + Sticker"
 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 if($message == "สวัสดี"){
       $arrayPostData['to'] = $id;
