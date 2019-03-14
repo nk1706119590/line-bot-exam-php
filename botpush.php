07f112dc2a9396cad71f7d6e5186d9e6ee6d5d7b
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 14:22:24 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -3,20 +3,16 @@
 require "vendor/autoload.php";
 
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
-
 $channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
 
 $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
   
-
 //$id = 'U5e9acf1216646459855f5735a974b170';
-
 $id = $_GET["id"];
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
 
 $response = $bot->getProfile($id);
 if ($response->isSucceeded()) {
@@ -27,15 +23,6 @@ if ($response->isSucceeded()) {
     echo "Status : " .$profile['statusMessage'];
       
 }
-
 //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 //$response = $bot->pushMessage($pushID, $textMessageBuilder);
-
 //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-
-
-
-
-
-
