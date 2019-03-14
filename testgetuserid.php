78460afa8c2d5c68d66ba9fe36b072d82d02adc3
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 09:33:15 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -27,12 +27,23 @@ if($message == "สวัสดี"){
 }
   
 function pushMsg($arrayHeader,$arrayPostData){
-      if($id != null){
+   $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+      /*if($id != null){
    echo "ไอดีเข้าจ้าาาาา";
 }
 else{
    echo "ไอดีไม่เข้าาาาาา ไปแก้ใหม่!!! ";
-}
+}*/
       
    
 }
