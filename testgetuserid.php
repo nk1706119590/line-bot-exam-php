b36297206dd823f66110097557845754af84488c
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 09:44:43 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -2,31 +2,28 @@
 
 require "vendor/autoload.php";
 
-$accessToken = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+$access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
+
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
-$content = file_get_contents('php://input');
- 
-// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
-$events = json_decode($content, true);
-if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-    $replyToken = $events['events'][0]['replyToken'];
+
+if(!is_null($arrayJson)){
+  $replyToken = $arrayJson['events'][0]['replyToken'];
 }
-// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
-$textMessageBuilder = new TextMessageBuilder(json_encode($events));
- 
-//l ส่วนของคำสั่งตอบกลับข้อความ
+
+$textMessageBuilder = new TextMessageBuilder(json_encode($arrayJson));
+
 $response = $bot->replyMessage($replyToken,$textMessageBuilder);
 if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
+    echo 'Succeeded!';
+    return;
 }
- 
-// Failed
+//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+//$response = $bot->pushMessage($pushID, $textMessageBuilder);
+//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
-
