0a5d6eec515fc830a0a6f7d9f15001f899bb133d
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 09:41:09 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -1,5 +1,7 @@
 <?php
-   
+
+require "vendor/autoload.php";
+
 $accessToken = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
 $channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
 
