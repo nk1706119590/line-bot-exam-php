d9afd8d663065b8e218277fa39e58d39e65293e2
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-14 09:24:34 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -5,9 +5,9 @@ $accessToken = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3
 $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
   
-$arrayHeader = array();
+/*$arrayHeader = array();
    $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";*/
 
    //รับข้อความจากผู้ใช้
 $message = $arrayJson['events'][0]['message']['text'];
@@ -23,9 +23,23 @@ if($message == "สวัสดี"){
      else{
         echo "อันยองงงง";
      }
+    //pushMsg($arrayHeader,$arrayPostData);
    
 }
-  
+ /* function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   
+}*/
 
    exit;
 ?>
