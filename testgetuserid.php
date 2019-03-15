df99a71b9de9365d62e48f09c9ceb84410582a9a
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-15 10:12:19 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -12,26 +12,11 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $id = $arrayJson['events'][0]['source']['userId'];
-/*if($id != ""){
-   echo "ไอดีเข้าจ้าาาาา";
-}
-else{
-   echo "ไอดีไม่เข้าาาาาา ไปแก้ใหม่!!! ";
-}*/
-//echo $id;
-/*$response = $bot->getProfile($id);
-echo "Respone" .$response;
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo "UserID : " .$profile['userId']."<br>";
-    echo "Name : " .$profile['displayName']."<br>";
-    echo "Pic : " .$profile['pictureUrl']."<br>";
-    echo "Status : " .$profile['statusMessage'];
-      
-}*/
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("https://lilyforlisa.herokuapp.com/botpush.php?id=".$id);
 $response1 = $bot->pushMessage($id, $textMessageBuilder);
+
 //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 //echo $textMessageBuilder;
+
 ?>
