f1999dad342db3b546a220b8a2b9daa4584a6b75
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-19 16:59:05 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -1,22 +1,16 @@
 <?php
 
-require "vendor/autoload.php";
+$headers = array('Content-Type: application/json','Authorization: Bearer <dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>');
+$ch = curl_init('https://api.line.me/v2/bot/profile/<U5e9acf1216646459855f5735a974b170>');
 
-$access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$content = file_get_contents('php://input');
-$arrayJson = json_decode($content, true);
+$profile_json = curl_exec($ch);
+$profile_array = json_decode($profile_json, true);
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-$id = $arrayJson['events'][0]['source']['userId'];
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("https://lilyforlisa.herokuapp.com/botpush.php?id=".$id);
-$response1 = $bot->pushMessage($id, $textMessageBuilder);
-
-//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-//echo $textMessageBuilder;
+print_r($profile_array);
+curl_close($ch);
 
 ?>
