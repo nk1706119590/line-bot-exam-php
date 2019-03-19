90cb63857b1c58d0bcb1dee5f570afd6a513a447
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-19 17:10:10 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,5 +1,35 @@
 <?php
 
+require "vendor/autoload.php";
+
+$access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'c70f0350f357af8e48b1d407eaf05db1';
+
+$content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+  
+$id = 'U5e9acf1216646459855f5735a974b170';
+
+//$id = $_GET["id"];
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$response = $bot->getProfile($id);
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo "UserID : " .$profile['userId']."<br>";
+    echo "Name : " .$profile['displayName']."<br>";
+    echo "Pic : " .$profile['pictureUrl']."<br>";
+    echo "Status : " .$profile['statusMessage'];
+      
+}
+/*$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($id);
+$response = $bot->pushMessage($id, $textMessageBuilder);*/
+//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+/*<?php
+
 $access_token = 'dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=';
 $cs ='c70f0350f357af8e48b1d407eaf05db1';
 
@@ -19,7 +49,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo $result;*/
 
 
 
