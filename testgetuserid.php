9bc42dc2165c7b40f84b8cd05edfe3deab7d8b3c
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-19 17:03:32 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -1,6 +1,6 @@
 <?php
 
-$headers = array('Content-Type: application/json','Authorization: Bearer <dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>');
+$headers = array('Content-Type: application/json', 'Authorization: Bearer <dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>');
 $ch = curl_init('https://api.line.me/v2/bot/profile/<U5e9acf1216646459855f5735a974b170>');
 
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
