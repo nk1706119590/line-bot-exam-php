7549117a0bd933b3a1565be4fd872ca22e7ae941
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-20 09:06:29 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -1,16 +1,22 @@
-<?
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACHURA
+ * Date: 20/3/2562
+ * Time: 8:42
+ */
 
-$headers = array('Content-Type: application/json', 'Authorization: Bearer <dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>');
-$ch = curl_init('https://api.line.me/v2/bot/profile/<U5e9acf1216646459855f5735a974b170>');
+$opts = [
+    "http" => [
+        "header" => "Content-Type: application/json\r\n"."Authorization: Bearer <dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=>"
+    ]
+];
 
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$context = stream_context_create($opts);
 
-$profile_json = curl_exec($ch);
+$profile_json = file_get_contents('https://api.line.me/v2/bot/profile/<U5e9acf1216646459855f5735a974b170>', false, $context);
 $profile_array = json_decode($profile_json, true);
-
 print_r($profile_array);
-curl_close($ch);
+
 
 ?>
