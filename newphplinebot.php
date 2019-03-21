b6c154affcc45d9ddd56449ca30674e441202c1d
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-03-21 15:51:23 +0700

    Update newphplinebot.php

--- newphplinebot.php
+++ newphplinebot.php
@@ -1,3 +1,5 @@
+
+
 <?php 
 	/*Get Data From POST Http Request*/
 	$datas = file_get_contents('php://input');
@@ -6,36 +8,41 @@
 
 	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
 
-	$replyToken = $deCode['events'][0]['replyToken'];
+	$replyToken = $deCode['events'][0]['replyToken']; // เก็บค่า replyToken ไว้
 
-	$messages = [];
-	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
+  if($messages == "555"){
+    $messages['replyToken'] = $replyToken; 
+	  $messages['messages'][0] = getFormatTextMessage("ตัวเลข"); 
+  }
 
-	$encodeJson = json_encode($messages);
+	/*$messages = []; // ข้อความที่ user ส่งเข้ามา
+	$messages['replyToken'] = $replyToken; 
+	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");*/ // ไปดู function getFormatMessage
 
-	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
-  	$LINEDatas['token'] = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU=";
+	$encodeJson = json_encode($messages); // แปลง Json เป็น array
 
-  	$results = sentMessage($encodeJson,$LINEDatas);
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply"; // เรียกใช้ api จาก Line
+  $LINEDatas['token'] = "dr0CTYutcnUKHQSfdWOv9yMQI1F3HljZfcHcIedbCuFft8kMzH7fGbaMspAqand3KD2bN2TqqubE1sYvCDGDDBrX3cDJ6lGdGZoFxoajJnBKsh+K4tz/fsflL71LpZ/fdXWg5ar7ppN8ycx1vUUKOwdB04t89/1O/w1cDnyilFU="; // channel token line bot
+
+  	$results = sentMessage($encodeJson,$LINEDatas); // ไปดูที่ function sendMessage
 
 	/*Return HTTP Request 200*/
 	http_response_code(200);
 
 	function getFormatTextMessage($text)
 	{
-		$datas = [];
+		$datas = []; // ตัวแปรที่รับค่าเข้ามา >> มาจากบรรทัดบนสุด
 		$datas['type'] = 'text';
-		$datas['text'] = $text;
+		$datas['text'] = $text; // นำข้อความมาเก็บไว้ใน $text
 
-		return $datas;
+		return $datas; // ส่งค่ากลับไปที่ $messages
 	}
 
 	function sentMessage($encodeJson,$datas)
 	{
 		$datasReturn = [];
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
+		$curl = curl_init(); //สร้าง curl resource
+		curl_setopt_array($curl, array( // set option ในที่นี้คือ array
 		  CURLOPT_URL => $datas['url'],
 		  CURLOPT_RETURNTRANSFER => true,
 		  CURLOPT_ENCODING => "",
@@ -72,3 +79,4 @@
 		return $datasReturn;
 	}
 ?>
+
