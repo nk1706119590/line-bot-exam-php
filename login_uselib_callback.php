7739358f269a1e859d78cebbd76dc123ae652392
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-19 15:42:01 +0700

    Update login_uselib_callback.php

--- login_uselib_callback.php
+++ login_uselib_callback.php
@@ -1,41 +1,22 @@
 <?php
-session_start();
-require_once("LineLoginLib.php");
- 
-// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
- 
-// กรณีมีการเชื่อมต่อกับฐานข้อมูล
-//require_once("dbconnect.php");
- 
-/// ส่วนการกำหนดค่านี้สามารถทำเป็นไฟล์ include แทนได้
-define('LINE_LOGIN_CHANNEL_ID','1553825543');
-define('LINE_LOGIN_CHANNEL_SECRET','b7fb69c64709f8b93f19df2c514940ce');
-define('LINE_LOGIN_CALLBACK_URL','https://lilyforlisa.herokuapp.com/login_uselib_callback.php');
- 
-$LineLogin = new LineLoginLib(
-    LINE_LOGIN_CHANNEL_ID, LINE_LOGIN_CHANNEL_SECRET, LINE_LOGIN_CALLBACK_URL);
-     
-// กรณีต้องการดึงค่าเฉพาะ access token ไปใช้งาน
-/*$accToken = $LineLogin->requestAccessToken($_GET);
-if(isset($accToken) && is_string($accToken)){
-    $_SESSION['ses_login_accToken_val'] = $accToken;
-}*/
- 
-// กรณีต้องการดึงค่าเฉพาะ access token และค่าอื่นๆ รวมถึงข้อมูลผู้ใช้ เช่น userId ในไลน์ ไปใช้งาน
-$dataToken = $LineLogin->requestAccessToken($_GET, true);
-if(!is_null($dataToken) && is_array($dataToken)){
-    if(array_key_exists('access_token',$dataToken)){
-        $_SESSION['ses_login_accToken_val'] = $dataToken['access_token'];
-    }
-    if(array_key_exists('refresh_token',$dataToken)){
-        $_SESSION['ses_login_refreshToken_val'] = $dataToken['refresh_token'];
-    }   
-    if(array_key_exists('id_token',$dataToken)){
-        $_SESSION['ses_login_userData_val'] = $dataToken['user'];
-    }       
-}
-$LineLogin->redirect('login_uselib.php');
+date_default_timezone_set(“Asia/Bangkok”);
+
+$date = date(“Y-m-d”);
+$time = date(“H:i:s”);
+
+$json = file_get_contents(‘php://input’);
+                          
+$request = json_decode($json, true);
+                          
+$queryText = $request[“queryResult”][“queryText”];
+                          
+$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
+                          
+$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
+                          
+$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
+                          
+fwrite($myfile,$log);
+fclose($myfile);
+
 ?>
