d52a62388c67a62b2558d12cf5808682a79ee2bc
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-23 16:56:05 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -64,6 +64,15 @@ $id = $arrayJson['events'][0]['source']['userId'];
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
+else{
+    arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "อะยัยหยออออ";
+        $arrayPostData['messages'][1]['type'] = "sticker";
+        $arrayPostData['messages'][1]['packageId'] = "11538";
+        $arrayPostData['messages'][1]['stickerId'] = "51626506";
+        replyMsg($arrayHeader,$arrayPostData);
+}
 
 function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
