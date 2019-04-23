7cfcb60a183d5c8bc8d45f51119ae2ad554f67c8
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-23 11:48:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -28,6 +28,9 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			
+			echo $text;
+
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -52,4 +55,4 @@ if (!is_null($events['events'])) {
 	}
 }
 
-echo $event;
+echo "5555";
