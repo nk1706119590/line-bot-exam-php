04bcb29fa58789b652f7792e33772d173d57e1a2
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-23 14:31:17 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -11,11 +11,13 @@
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
 
+$id = $arrayJson['events'][0]['source']['userId'];
+
 #ตัวอย่าง Message Type "Text"
     if($message == "สวัสดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา".$id;
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
