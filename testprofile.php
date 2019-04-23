50cb0119a800b38b037b4706f9c2aa255a790fe4
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-23 14:34:07 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -1,6 +1,7 @@
 <?php
     $accessToken = "JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-    
+    $channelSecret = '7410da12768dbb3db2632dd64ed33a12';    
+
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
     
@@ -12,6 +13,19 @@
     $message = $arrayJson['events'][0]['message']['text'];
 
 $id = $arrayJson['events'][0]['source']['userId'];
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$response = $bot->getProfile($id);
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo "UserID : " .$profile['userId']."<br>";
+    echo "Name : " .$profile['displayName']."<br>";
+    echo "Pic : " .$profile['pictureUrl']."<br>";
+    echo "Status : " .$profile['statusMessage'];
+      
+}
 
 #ตัวอย่าง Message Type "Text"
     if($message == "สวัสดี"){
