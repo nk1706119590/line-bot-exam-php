86b558b323c79184a4e4c2cb94449ca0bb58cde2
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-23 13:37:47 +0700

    Update testgetuserid.php

--- testgetuserid.php
+++ testgetuserid.php
@@ -7,9 +7,6 @@ $channelSecret = '7410da12768dbb3db2632dd64ed33a12';
 $content = file_get_contents('php://input');
 $arrayJson = json_decode($content, true);
 
-   $arrayHeader = array();
-    $arrayHeader[] = "Content-Type: application/json";
-    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
@@ -25,11 +22,18 @@ $id = $arrayJson['events'][0]['source']['userId'];
  }*/
 
  if($message == "สวัสดี"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "555>>> ".$id;
-        replyMsg($arrayHeader,$arrayPostData);
+    
+    $response = $bot->getProfile($id);
+      if ($response->isSucceeded()) {
+          $profile = $response->getJSONDecodedBody();
+         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("".$profile['displayName']);
+    $response1 = $bot->pushMessage($id, $textMessageBuilder);
+          echo "UserID : " .$profile['userId']."<br>";
+          echo "Name : " .$profile['displayName']."<br>";
+          echo "Pic : " .$profile['pictureUrl']."<br>";
+          echo "Status : " .$profile['statusMessage'];
  }
+
 
 
 /*if($id != ""){
@@ -53,18 +57,4 @@ if ($response->isSucceeded()) {
 
 //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 //echo $textMessageBuilder;
-function replyMsg($arrayHeader,$arrayPostData){
-        $strUrl = "https://api.line.me/v2/bot/message/reply";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$strUrl);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close ($ch);
-    }
-   exit;
 ?>
