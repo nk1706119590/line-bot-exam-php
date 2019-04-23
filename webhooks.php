87c82c176f8c74750ca980a8672c576befb559ea
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-23 11:17:04 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			echo $result . "\r\n";
 			
-			$_GET = 'https://lilyforlisa.herokuapp.com/profile.php?id=$text';
+			POST = 'https://lilyforlisa.herokuapp.com/profile.php?id=$text';
 		}
 	}
 }
