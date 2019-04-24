5874a7891743bcd72edb9e23b853e7b5b46fc907
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-24 16:19:15 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,6 @@
-<?php$access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU='
+<?php
+
+  $access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU='
   
   ;$url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
@@ -12,3 +14,4 @@
   curl_close($ch);
 
 echo $result;
+
