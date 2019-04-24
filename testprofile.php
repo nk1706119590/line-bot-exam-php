da3cde7312ca78cc79e09e0fb0ba5b668d37e368
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-24 08:38:05 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -2,6 +2,9 @@
     $accessToken = "JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     $channelSecret = '7410da12768dbb3db2632dd64ed33a12';    
 
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
     
@@ -13,8 +16,7 @@
     $message = $arrayJson['events'][0]['message']['text'];
     $id = $arrayJson['events'][0]['source']['userId'];
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
 $response = $bot->getProfile($id);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
