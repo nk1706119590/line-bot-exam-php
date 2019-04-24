142426ba4cc095dadb48d9ddef849992b5c5cb79
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-24 16:34:13 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,17 +1,13 @@
 <?php
 
-  $access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU='
+  $access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=';
   
-  ;$url = 'https://api.line.me/v1/oauth/verify';
-  $headers = array('Authorization: Bearer ' . $access_token);
-
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-
-  $result = curl_exec($ch);
-  curl_close($ch);
-
+    $url = 'https://api.line.me/v1/oauth/verify';
+    $headers = array('Authorization: Bearer ' . $access_token);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
 echo $result;
-
