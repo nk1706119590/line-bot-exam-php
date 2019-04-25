72f5dc0811d89fe62f8fc47bdbd834e0a230aea2
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-25 09:38:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -63,82 +63,47 @@ if(!is_null($events)){
     $userMessage = $events['events'][0]['message']['text'];
     $userMessage = strtolower($userMessage);
     switch ($typeMessage){
-        case 'text':
-            switch ($userMessage) {
-                case "t":
-                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
-                    $replyData = new TextMessageBuilder($textReplyMessage);
-                    break;
-                case "i":
-                    $picFullSize = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower';
-                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower/240';
-                    $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
-                    break;
-                case "v":
-                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
-                    $videoUrl = "https://www.mywebsite.com/simplevideo.mp4";                
-                    $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
-                    break;
-                case "a":
-                    $audioUrl = "https://www.mywebsite.com/simpleaudio.mp3";
-                    $replyData = new AudioMessageBuilder($audioUrl,27000);
-                    break;
-                case "l":
-                    $placeName = "ที่ตั้งร้าน";
-                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-                    $latitude = 13.780401863217657;
-                    $longitude = 100.61141967773438;
-                    $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);              
-                    break;
-                case "s":
-                    $stickerID = 22;
-                    $packageID = 2;
-                    $replyData = new StickerMessageBuilder($packageID,$stickerID);
-                    break;      
-                case "im":
-                    $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
-                    $replyData = new ImagemapMessageBuilder(
-                        $imageMapUrl,
-                        'This is Title',
-                        new BaseSizeBuilder(699,1040),
-                        array(
-                            new ImagemapMessageActionBuilder(
-                                'test image map',
-                                new AreaBuilder(0,0,520,699)
-                                ),
-                            new ImagemapUriActionBuilder(
-                                'http://www.ninenik.com',
-                                new AreaBuilder(520,0,520,699)
-                                )
-                        )); 
-                    break;          
-                case "tm":
-                    $replyData = new TemplateMessageBuilder('Confirm Template',
-                        new ConfirmTemplateBuilder(
-                                'Confirm template builder',
-                                array(
-                                    new MessageTemplateActionBuilder(
-                                        'Yes',
-                                        'Text Yes'
-                                    ),
-                                    new MessageTemplateActionBuilder(
-                                        'No',
-                                        'Text NO'
-                                    )
-                                )
-                        )
-                    );
-                    break;                                                                                                                          
-                default:
-                    $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
-                    $replyData = new TextMessageBuilder($textReplyMessage);         
-                    break;                                      
-            }
-            break;
-        default:
-            $textReplyMessage = json_encode($events);
-            $replyData = new TextMessageBuilder($textReplyMessage);         
-            break;  
+        case "t_b":
+        // กำหนด action 4 ปุ่ม 4 ประเภท
+        $actionBuilder = array(
+            new MessageTemplateActionBuilder(
+                'Message Template',// ข้อความแสดงในปุ่ม
+                'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+            ),
+            new UriTemplateActionBuilder(
+                'Uri Template', // ข้อความแสดงในปุ่ม
+                'https://www.ninenik.com'
+            ),
+            new DatetimePickerTemplateActionBuilder(
+                'Datetime Picker', // ข้อความแสดงในปุ่ม
+                http_build_query(array(
+                    'action'=>'reservation',
+                    'person'=>5
+                )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
+                substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
+                substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
+                substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
+            ),      
+            new PostbackTemplateActionBuilder(
+                'Postback', // ข้อความแสดงในปุ่ม
+                http_build_query(array(
+                    'action'=>'buy',
+                    'item'=>100
+                )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+            ),      
+        );
+        $imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
+        $replyData = new TemplateMessageBuilder('Button Template',
+            new ButtonTemplateBuilder(
+                    'button template builder', // กำหนดหัวเรื่อง
+                    'Please select', // กำหนดรายละเอียด
+                    $imageUrl, // กำหนด url รุปภาพ
+                    $actionBuilder  // กำหนด action object
+            )
+        );              
+        break;  
     }
 }
 //l ส่วนของคำสั่งตอบกลับข้อความ
