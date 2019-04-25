de6fee248067fdcf8ac891122702d4bcdb142ca3
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-25 09:23:01 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+  /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+  define('LINE_MESSAGE_CHANNEL_ID','1566493805');
+  define('LINE_MESSAGE_CHANNEL_SECRET','7410da12768dbb3db2632dd64ed33a12');
+  define('LINE_MESSAGE_ACCESS_TOKEN','JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=');
+?>
