f72228b131d02c7a2a737d1e39c0b5bfe93a27a0
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-04-29 16:30:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -116,20 +116,8 @@ if(!is_null($events)){
                     );              
                     break;
                 case "ประชาสัมพันธ์":
-                $actionBuilder = array(
-                        new UriTemplateActionBuilder(
-                            'เข้าสู่เว็บไซต์', // ข้อความแสดงในปุ่ม
-                            'https://manager.line.biz/account/@578priml/richmenu'
-                        ), 
-                     $imageUrl = 'https://3.bp.blogspot.com/-AEczW23WrA8/V7NMIIdxUwI/AAAAAAAAAHQ/3312FluUinUjlx7xkjl2Mjeki9H0FQxDwCLcB/s1600/contact.jpg';
-                    $replyData = new TemplateMessageBuilder('Button Template',
-                        new ButtonTemplateBuilder(
-                                'ติดต่อเรา', // กำหนดหัวเรื่อง
-                                ' ', // กำหนดรายละเอียด
-                                $imageUrl, // กำหนด url รุปภาพ
-                                $actionBuilder  // กำหนด action object
-                        )
-                    ); 
+                $textReplyMessage = " test ";
+                    $replyData = new TextMessageBuilder($textReplyMessage);    
                     break;
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
