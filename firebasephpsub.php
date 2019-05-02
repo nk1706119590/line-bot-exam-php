d31ccfa4f19efe01479352c49516135f61fddb0b
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-02 15:24:19 +0700

    Create firebasephpsub.php

---
+++ firebasephpsub.php
@@ -0,0 +1,42 @@
+< ? php
+
+
+
+require __DIR__.
+
+'/vendor/autoload.php';
+
+require 'firebasephp.php';
+
+
+use Kreait\ Firebase\ Factory;
+
+use Kreait\ Firebase\ ServiceAccount;
+
+
+$users = new Users();
+
+
+if (isset($_POST['submit'])) {
+
+
+   $name = $_POST['name'];
+
+   $email = $_POST['email'];
+
+   $message = $_POST['message'];
+
+
+   $users - > insert([
+
+       'name' => $name,
+
+       'email' => $email,
+
+       'message' => $message
+
+   ]);
+
+
+
+}
