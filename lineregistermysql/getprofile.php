52482d9cda584fb306b24014d82323acf8555254
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 15:15:58 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -1,13 +1,4 @@
 <?php
-
-require "vendor/autoload.php";
-
-$access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '7410da12768dbb3db2632dd64ed33a12';
-
-$content = file_get_contents('php://input');
-$arrayJson = json_decode($content, true);
-
     $serverName="1.179.246.109";
     $userName="root";
     $userPassword="cctsssystem";
@@ -19,16 +10,10 @@ $arrayJson = json_decode($content, true);
     $result = mysqli_query($con, $query); 
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-$response = $bot->getProfile($query);
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo "UserID : " .$profile['userId']."<br>";
-    echo "Name : " .$profile['displayName']."<br>";
-    echo "Pic : " .$profile['pictureUrl']."<br>";
-    echo "Status : " .$profile['statusMessage'];
+    echo "UserID : "['userId']."<br>";
+    echo "Name : "['name']."<br>";
+    echo "Address : "['address']."<br>";
+    echo "Phonenumber : "['phonenumber'];
       
 }
 ?>
