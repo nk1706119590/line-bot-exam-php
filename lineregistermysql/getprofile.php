e83466dd569095bfeb29a4201c56cd721276f20e
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 16:07:04 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -12,13 +12,14 @@
 
     
     $id = "SELECT user_id FROM register";
+$resultt = mysqli_query($con, $id); 
 
     $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 
     $query = "SELECT * FROM register" or die("Error:" . mysqli_error()); 
     $result = mysqli_query($con, $query); 
 
-    if($id != null){        
+    if($resultt != null){        
             echo "<table border='1' align='center' width='500'>";
     //หัวข้อตาราง
     echo "<tr align='center' bgcolor='#CCCCCC'><td>รหัส</td><td>ชื่อ-นามสกุล</td><td>ที่อยู่</td><td>เบอร์ติดต่อ</td></tr>";
