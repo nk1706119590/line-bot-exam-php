e681262c5a109a728e9ae3236f1d072f95c7343e
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 13:59:56 +0700

    Create getprofile.php

---
+++ lineregistermysql/getprofile.php
@@ -0,0 +1,34 @@
+<?php
+
+    date_default_timezone_set("Asia/Bangkok");
+    
+    $date = date("Y-m-d");
+    $time = date("H:i:s");
+    
+    $serverName="1.179.246.109";
+    $userName="root";
+    $userPassword="cctsssystem";
+    $dbName="db_uuline_test";
+    
+    $name = $_REQUEST['name'];
+    $address = $_REQUEST['address'];
+    $phonenumber = $_REQUEST['phonenumber'];
+    $id = $_REQUEST['userid'];
+    
+    $connect=mysqli_connect($serverName,$userName,$userPassword,$dbName)or die("connecterror".mysqli_error());
+    mysqli_set_charset($connect,"utf8"); 
+    
+    $sql = "select user_id from register where user_id='$id' group by user_id"; 
+    $result = mysqli_query($connect,$sql) or die ("error".mysqli_error()); 
+    $count_row = mysqli_num_rows($result);
+    
+        if($count_row < 1){        
+            $query = "select * from register where user_id='$id' group by user_id""; 
+            $resource = mysqli_query($connect,$query) or die ("error".mysqli_error());
+            
+          
+        }
+        else{ 
+            
+        } 
+?>
