80c88433221cf6bf39071bd5391309654a2beb5b
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 15:23:26 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -1,27 +1,35 @@
 <?php
+
+    date_default_timezone_set("Asia/Bangkok");
+    
+    $date = date("Y-m-d");
+    $time = date("H:i:s");
     
     $serverName="1.179.246.109";
     $userName="root";
     $userPassword="cctsssystem";
     $dbName="db_uuline_test";
 
-    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName)or die("connecterror".mysqli_error());
-    mysqli_set_charset($con,"utf8"); 
+    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 
-    $query = "SELECT user_id FROM register" or die("Error:" . mysqli_error()); 
-
+    $query = "SELECT * FROM register ORDER BY id asc" or die("Error:" . mysqli_error()); 
     $result = mysqli_query($con, $query); 
 
 
-    echo "UserID : " . ["userId"] . "<br>";
-    
-      
-}
+    echo "<table border='1' align='center' width='500'>";
+    //หัวข้อตาราง
+    echo "<tr align='center' bgcolor='#CCCCCC'><td>รหัส</td><td>ชื่อ-นามสกุล</td><td>ที่อยู่</td><td>เบอร์ติดต่อ</td></tr>";
+
+    while($row = mysqli_fetch_array($result)) { 
+      echo "<tr>";
+      echo "<td>" .$row["user_id"] .  "</td> "; 
+      echo "<td>" .$row["name"] .  "</td> ";  
+      echo "<td>" .$row["address"] .  "</td> ";
+      echo "<td>" .$row["phonenumber"] .  "</td> ";
+
+    }
+echo "</table>";
+//close connection
 mysqli_close($con);
 ?>
-
-
-
-
-
 
