3c5cac070acc4ded642dd9b02c5a01219a2bb8f9
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 15:07:04 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -9,30 +9,28 @@
     $userName="root";
     $userPassword="cctsssystem";
     $dbName="db_uuline_test";
-    /*
-    $user_id = null;
-
-    if(isset($_GET["user_id"])){
-        $user_id = $_GET["user_id"];
-    }*/
 
     $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
 
-    $sql = "SELECT * FROM register ORDER BY id asc";
+    $query = "SELECT * FROM register ORDER BY id asc" or die("Error:" . mysqli_error()); 
+//เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result . 
+    $result = mysqli_query($con, $query); 
+// แสดงข้อมูลที่ query ออกมา โดยใช้ตารางในการจัดข้อมูล: 
 
-    $query = mysqli_query($con,$sql);
-    $result = mysqli_fetch_array($query,MYSQLI_ASSOC);
+    echo "<table border='1' align='center' width='500'>";
+    //หัวข้อตาราง
+    echo "<tr align='center' bgcolor='#CCCCCC'><td>รหัส</td><td>ชื่อ-นามสกุล</td><td>ที่อยู่</td>เบอร์ติดต่อ<td></td></tr>";
 
-    echo "<br/><br/>";
-    echo $result["user_id"];
-    echo "<br/><br/>";
-    echo $result["name"];
-    echo "<br/><br/>";
-    echo $result["address"];
-    echo "<br/><br/>";
-    echo $result["phonenumber"];
+    while($row = mysqli_fetch_array($result)) { 
+      echo "<tr>";
+      echo "<td>" .$row["user_id"] .  "</td> "; 
+      echo "<td>" .$row["name"] .  "</td> ";  
+      echo "<td>" .$row["address"] .  "</td> ";
+      echo "<td>" .$row["phonenumber"] .  "</td> ";
 
-
-mysqli_close($conn);
-
+    }
+echo "</table>";
+//close connection
+mysqli_close($con);
 ?>
+
