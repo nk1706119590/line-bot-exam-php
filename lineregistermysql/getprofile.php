085b771030867c227e545a1746de2e1cbd5b0eb0
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 15:18:12 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -1,19 +1,20 @@
 <?php
+    
     $serverName="1.179.246.109";
     $userName="root";
     $userPassword="cctsssystem";
     $dbName="db_uuline_test";
 
-    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName)or die("connecterror".mysqli_error());
+    mysqli_set_charset($con,"utf8"); 
+
     $query = "SELECT user_id FROM register" or die("Error:" . mysqli_error()); 
 
     $result = mysqli_query($con, $query); 
 
 
     echo "UserID : "['userId']."<br>";
-    echo "Name : "['name']."<br>";
-    echo "Address : "['address']."<br>";
-    echo "Phonenumber : "['phonenumber'];
+    
       
 }
 ?>
