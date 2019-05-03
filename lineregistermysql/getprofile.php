af793285bcd97ad3efd475e35a2f513060200c51
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-03 14:32:27 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -10,23 +10,25 @@
     $userPassword="cctsssystem";
     $dbName="db_uuline_test";
     
-    $name = $_REQUEST['name'];
-    $address = $_REQUEST['address'];
-    $phonenumber = $_REQUEST['phonenumber'];
-    $id = $_REQUEST['userid'];
-    
-    $connect=mysqli_connect($serverName,$userName,$userPassword,$dbName)or die("connecterror".mysqli_error());
-    mysqli_set_charset($connect,"utf8"); 
-    
-    $sql = "select * from register where user_id='$id' group by user_id"; 
-    /*$result = mysqli_query($connect,$sql) or die ("error".mysqli_error()); */
+    $user_id = null;
 
-    if(!$resultt = $connect->query($sql)){
-        die('Then was an error running the query ['. $connect->error . ']');
+    if(isset($_GET["user_id"])){
+        $user_id = $_GET["user_id"];
     }
 
-    while($row = $resultt->fetch_assoc()){
-        echo $row['user_id'] . ', ' . $row['name'] . ', ' . $row['address'] . ', ' . $row['phonenumber'] . '<br />';
-    }
+    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+
+    $sql = "SELECT * FROM register WHERE user_id='".$user_id."'";
+
+    $query = mysqli_query($con,$sql);
+    $result = mysqli_fetch_array($query,MYSQLI_ASSOC);
+
+    echo $result["user_id"];
+    echo $result["name"];
+    echo $result["address"];
+    echo $result["phonenumber"];
+
+
+mysqli_close($conn);
 
 ?>
