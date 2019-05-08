eecc0e11ae865538234eeeb09ba8b57b6e41cb5a
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-08 09:22:03 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -1,10 +1,9 @@
 <?php
-
     date_default_timezone_set("Asia/Bangkok");
-    
+
     $date = date("Y-m-d");
     $time = date("H:i:s");
-    
+
     $serverName="1.179.246.109";
     $userName="root";
     $userPassword="cctsssystem";
@@ -14,36 +13,25 @@
     $address = $_REQUEST['address'];
     $phonenumber = $_REQUEST['phonenumber'];
     $id = $_REQUEST['userid'];
-
-    $con = mysqli_connect($serverName,$userName,$userPassword,$dbName);
     
-    $query = "SELECT * FROM register where user_id='$id'" or die("Error:" . mysqli_error()); 
-    $result = mysqli_query($con, $query); 
+    $connect=mysqli_connect($serverName,$userName,$userPassword,$dbName)or die("connecterror".mysqli_error());
+    mysqli_set_charset($connect,"utf8"); 
+
+    $sql = "select user_id from register where user_id='$id' group by user_id"; 
+    $result = mysqli_query($connect,$sql) or die ("error".mysqli_error()); 
     $count_row = mysqli_num_rows($result);
 
-    if($count_row > 0){
-        while($res = mysqli_fetch_array($result)){
-            echo "Name : " .$res['name'] . "</br>";
-        }  
-    }
-    else {
-        echo "คุณยังไม่ได้ลงทะเบียน";
-    }
-
-    /*echo "<table border='1' align='center' width='500'>";
-    //หัวข้อตาราง
-    echo "<tr align='center' bgcolor='#CCCCCC'><td>รหัส</td><td>ชื่อ-นามสกุล</td><td>ที่อยู่</td><td>เบอร์ติดต่อ</td></tr>";
-
-        while($row = mysqli_fetch_array($result)) { 
-          echo "<tr>";
-          echo "<td>" .$row["user_id"] .  "</td> "; 
-          echo "<td>" .$row["name"] .  "</td> ";  
-          echo "<td>" .$row["address"] .  "</td> ";
-          echo "<td>" .$row["phonenumber"] .  "</td> ";
-
+        if($count_row < 1){        
+            $query = "INSERT INTO register(user_id,name,address,phonenumber,date) VALUE ('$id', '$name','$address','$phonenumber',NOW())"; 
+            $resource = mysqli_query($connect,$query) or die ("error".mysqli_error());
+            
+            echo "<br/><br/>";
+            echo '<h1 align="center"><font color="red">*** ยินดีด้วย คุณลงทะเบียนสำเร็จแล้ว ***</font></h1>';
+            echo '<h1 align="center"><font color="red"> กดที่เครื่องหมาย X มุมขวาบนเพื่อปิดหน้าต่างนี้</font></h1>'; 
         }
-    echo "</table>";*/
-//close connection
-mysqli_close($con);
+        else{ 
+            echo "Name : " ;
+            echo '<h1 align="center"><font color="red">*** ขอโทษด้วย คุณเคยลงทะเบียนแล้ว ***</font></h1>';
+            echo '<h1 align="center"><font color="red"> กดที่เครื่องหมาย X มุมขวาบนเพื่อปิดหน้าต่างนี้</font></h1>';
+        } 
 ?>
-
