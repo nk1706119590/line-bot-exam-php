958174fe5510d60f69b387ad768a2bfa568128a3
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-08 12:27:18 +0700

    Update getprofile.php

--- lineregistermysql/getprofile.php
+++ lineregistermysql/getprofile.php
@@ -15,6 +15,8 @@
     $phonenumber = $_REQUEST['phonenumber'];
     $id = $_REQUEST['userid'];
 
+    $access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=';
+    $channelSecret = '7410da12768dbb3db2632dd64ed33a12';
     
     $connect=mysqli_connect($serverName,$userName,$userPassword,$dbName)or die("connecterror".mysqli_error());
     mysqli_set_charset($connect,"utf8"); 
