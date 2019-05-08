7fba69a24d586444272ea2086d67ad4d4aab4947
nk1706119590 <48193352+nk1706119590@users.noreply.github.com>
2019-05-08 12:29:59 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -5,6 +5,20 @@ require "vendor/autoload.php";
 $access_token = 'JOALJaFzXSS1/Iw0lRElqFUMiBHUF4LhFisSOpo9WpfG4Ju5l+o+o5yTWeYVIqOhwPafmf63J283XV1uMahQlwgdfCxzlKipJygVt7h4z9Fbt0mq+eQivXcy4jj4oyvvH8a6cp39m8SO/3I9OyLmVgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '7410da12768dbb3db2632dd64ed33a12';
 
+
+    $date = date("Y-m-d");
+    $time = date("H:i:s");
+
+    $serverName="1.179.246.109";
+    $userName="root";
+    $userPassword="cctsssystem";
+    $dbName="db_uuline_test";
+
+    $name = $_REQUEST['name'];
+    $address = $_REQUEST['address'];
+    $phonenumber = $_REQUEST['phonenumber'];
+    $id = $_REQUEST['userid'];
+
 $content = file_get_contents('php://input');
 $arrayJson = json_decode($content, true);
 
